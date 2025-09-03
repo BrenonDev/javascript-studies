02c987fdbfbcfb8d62f11d1044668b223f708987
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-09-02 21:54:16 -0300

    var e case-sensitive

---
+++ 2 - Variáveis e Constantes/scripts.js
@@ -0,0 +1,31 @@
+// VAR
+
+// Declarar uma variável sem valor
+var user
+
+console.log(user)
+
+// Declarar uma variável com valor
+
+var email = "brenon.rondello@gmail.com"
+
+console.log(email)
+
+// Substituir o valor da variável
+
+email = "brenon.oliverri.rondello@gmail.com"
+
+console.log(email)
+
+// ======================================================================
+
+// CASE-SENSITIVE
+
+// Quando uma linguagem de programação é case-sensitive significa que ela é sensível a letras maiúsculas e minúsculas.
+
+var product = "Teclado Mecânico"
+var Product = "Mouse Gamer"
+
+console.log(product)
+console.log(Product)
+

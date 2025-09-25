3e2f14b2b066dbc298ff3daac80f6bb46f2e9798
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-09-24 21:16:15 -0300

    expressões e operadores

---
+++ 3 - Expressões e Operadores/scripts.js
@@ -0,0 +1,12 @@
+console.log("=== EXPRESSÕES E OPERADORES ===")
+
+// Expressões e Operadores são fundamentais em JavaScript para realizar cálculos, manipular dados e controlar o fluxo do programa.
+
+// Operadores são símbolos que realizam operações em um ou mais operandos (valores, variáveis, etc.). As expressões são combinações de valores, variáveis e operadores que produzem um valor.
+
+// Expressão é a combinação de valores, variáveis e operadores que resulta em um valor. Por exemplo, 5 + 3 é uma expressão que resulta em 8.
+
+
+// ======================================================================
+
+

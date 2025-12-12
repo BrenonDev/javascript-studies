c896ab1fdc41c42af123c29e0b0c6462dcb61761
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-12-12 14:44:51 -0300

    parâmetros e argumentos

--- 5 - Funções/scripts.js
+++ 5 - Funções/scripts.js
@@ -3,8 +3,22 @@ console.log("=== FUNÇÕES ===")
 // Funções são blocos de código reutilizáveis que realizam uma tarefa específica. Elas podem receber parâmetros de entrada, executar operações e retornar um valor.
 
 // Declaração de uma função
-function message(nome) {
-    console.log("Olá, " + nome + "!")
+function ola() {
+    console.log("Hello, World!")
 }
 // Chamada da função
-message("Brenon")
+ola()
+
+
+// ======================================================================
+
+
+console.log("=== PARÂMETROS E ARGUMENTOS ===")
+
+// Funções podem receber parâmetros para trabalhar com dados fornecidos durante a chamada da função.
+function saudacao(nome) {
+    console.log("Olá, " + nome + "!")
+}
+
+// Chamada da função com argumento
+saudacao("Maria")

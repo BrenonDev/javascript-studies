00084123c3913e829c0c0058cad3ecfca828033e
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-12-12 10:33:10 -0300

    switch case

--- 4 - Condicionais e Controle de Fluxo/scripts.js
+++ 4 - Condicionais e Controle de Fluxo/scripts.js
@@ -66,3 +66,41 @@ if (nota >= 90) {
 } else {
     console.log("Reprovado!")
 }
+
+
+// ======================================================================
+
+
+console.log("=== SWITCH CASE ===")
+
+// A estrutura switch case é usada para executar diferentes blocos de código com base no valor de uma expressão.
+
+let diaSemana = 3
+let nomeDia = ""
+
+switch (diaSemana) {
+    case 1:
+        nomeDia = "Domingo"
+        break
+    case 2:
+        nomeDia = "Segunda-feira"
+        break
+    case 3:
+        nomeDia = "Terça-feira"
+        break
+    case 4:
+        nomeDia = "Quarta-feira"
+        break
+    case 5:
+        nomeDia = "Quinta-feira"
+        break
+    case 6:
+        nomeDia = "Sexta-feira"
+        break
+    case 7:
+        nomeDia = "Sábado"
+        break
+    default:
+        nomeDia = "Dia inválido"
+}
+console.log("O dia da semana é:", nomeDia)

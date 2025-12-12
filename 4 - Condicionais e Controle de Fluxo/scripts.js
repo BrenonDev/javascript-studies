f70aa55be0f955b9083d487a7ea33d65ecef61f9
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-12-12 09:23:52 -0300

    operador condicional ternário

--- 4 - Condicionais e Controle de Fluxo/scripts.js
+++ 4 - Condicionais e Controle de Fluxo/scripts.js
@@ -3,3 +3,16 @@ console.log("=== CONDICIONAIS ===")
 // Condicionais permitem executar diferentes blocos de código com base em condições específicas, sendo fundamentais para o controle de fluxo em programas.
 
 
+// ======================================================================
+
+
+console.log("=== OPERADOR CONDICIONAL TERNÁRIO ===")
+
+// O Operador Condicional Ternário é uma forma concisa de expressar uma condição if-else em uma única linha.
+
+let idade = 20
+let podeBeber = (idade >= 18) ? "Pode beber" : "Não pode beber"
+console.log("Idade: ", idade)
+console.log("Resultado do operador ternário: ", podeBeber)
+
+

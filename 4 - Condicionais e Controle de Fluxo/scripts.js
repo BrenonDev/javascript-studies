0c23b4245ebb6a25e65eda5930f706c99a6a0d9e
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-12-12 09:07:42 -0300

    condicionais

---
+++ 4 - Condicionais e Controle de Fluxo/scripts.js
@@ -0,0 +1,5 @@
+console.log("=== CONDICIONAIS ===")
+
+// Condicionais permitem executar diferentes blocos de código com base em condições específicas, sendo fundamentais para o controle de fluxo em programas.
+
+

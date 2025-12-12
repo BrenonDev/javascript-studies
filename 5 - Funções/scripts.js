e84c0a58b3f5976ab64b6c06497082e1fbfda8b1
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-12-12 14:35:50 -0300

    funções

---
+++ 5 - Funções/scripts.js
@@ -0,0 +1,10 @@
+console.log("=== FUNÇÕES ===")
+
+// Funções são blocos de código reutilizáveis que realizam uma tarefa específica. Elas podem receber parâmetros de entrada, executar operações e retornar um valor.
+
+// Declaração de uma função
+function message(nome) {
+    console.log("Olá, " + nome + "!")
+}
+// Chamada da função
+message("Brenon")

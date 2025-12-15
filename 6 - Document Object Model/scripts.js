8d75868a036688f250888cf3cbb5e5211403cebc
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-12-15 09:28:30 -0300

    document object model

---
+++ 6 - Document Object Model/scripts.js
@@ -0,0 +1,17 @@
+console.log("=== DOCUMENT OBJECT MODEL (DOM) ===")
+
+// O Document Object Model (DOM) é uma interface de programação para documentos HTML e XML. Ele representa a página para que os programas possam alterar a estrutura, estilo e conteúdo do documento.
+// O DOM representa o documento como uma árvore de nós, onde cada nó é um objeto que representa uma parte do documento.
+// Com o DOM, você pode usar linguagens de programação, como JavaScript, para manipular elementos HTML dinamicamente.
+
+// Exemplo de árvore DOM simplificada:
+
+//                Document
+//                   |
+//             Element <html>
+//              /          \
+//  Element <head>         Element <body>
+//             |           /            \
+//  Element <title>   Element <h1>     Element <p>
+//             |          |              |
+//  Text ("Site")     Text ("Título")  Text ("Hello, World!")

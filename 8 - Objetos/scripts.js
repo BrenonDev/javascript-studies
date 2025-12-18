57ea8493ce7a7a82d89b6b2dd8f926dbacc44c52
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-12-18 10:04:24 -0300

    objetos

---
+++ 8 - Objetos/scripts.js
@@ -0,0 +1,7 @@
+console.log("=== OBJETOS ===");
+
+// Um objeto é uma estrutura que agrupa dados (propriedades) e comportamentos (métodos) relacionados — normalmente uma instância concreta de um tipo ou classe.
+
+// Propriedades: campos que armazenam o estado de um objeto; representam atributos/valores.
+
+// Métodos: funções associadas a um objeto que definem comportamentos; podem ler/alterar propriedades.

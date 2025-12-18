bd197f77d3e5ff822f3eba2d2c7c559daa834d03
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-12-18 10:15:44 -0300

    criando um objeto

--- 8 - Objetos/scripts.js
+++ 8 - Objetos/scripts.js
@@ -5,3 +5,36 @@ console.log("=== OBJETOS ===");
 // Propriedades: campos que armazenam o estado de um objeto; representam atributos/valores.
 
 // Métodos: funções associadas a um objeto que definem comportamentos; podem ler/alterar propriedades.
+
+
+// ======================================================================
+
+
+console.log("=== CRIANDO UM OBJETO ===");
+
+// Cria um objeto vazio
+const obj = {};
+console.log(obj);
+console.log(typeof obj);
+
+// Cria um objeto com propriedades e métodos
+const user = {
+    email: "brenon.olivetti.rondello@gmail.com",
+    age: 18,
+    name: {
+        first_name: "Brenon",
+        last_name: "Olivetti Rondello",
+    },
+    address: {
+        street: "Rua Aurora Torres Cazerta",
+        number: 110,
+        city: "Sorocaba",
+        postal_code: "18051-841",
+    },
+    message: () => {
+        console.log("Hello World!");
+    },
+};
+
+
+// ======================================================================

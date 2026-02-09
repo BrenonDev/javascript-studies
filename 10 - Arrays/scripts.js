f0ee14e038aba5d8cc654b6102a1a3d39d460635
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-02-09 14:12:16 -0300

    conceito de array

---
+++ 10 - Arrays/scripts.js
@@ -0,0 +1,6 @@
+console.log("=== ARRAYS ===");
+
+// Arrays são estruturas de dados que armazenam uma coleção de elementos, podendo ser de qualquer tipo, como números, strings, objetos, etc. Em JavaScript, os arrays são criados usando colchetes [] e os elementos são separados por vírgulas. Eles possuem uma série de métodos e propriedades que facilitam a manipulação dos dados, como adicionar, remover, ordenar, filtrar, entre outros. Os arrays são indexados, ou seja, cada elemento tem um índice numérico que começa em 0.
+
+
+// ======================================================================

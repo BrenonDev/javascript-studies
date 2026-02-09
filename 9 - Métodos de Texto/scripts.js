ed323998d4c81520442719772e94fd941d6a87b8
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-02-09 13:11:09 -0300

    texto maiúsculo e minúsculo

---
+++ 9 - Métodos de Texto/scripts.js
@@ -0,0 +1,9 @@
+console.log("=== MAIÚSCULAS E MINÚSCULAS ===");
+
+const text = "Hello World!";
+
+// Converte o texto para maiúsculas
+console.log(text.toUpperCase());
+
+// Converte o texto para minúsculas
+console.log(text.toLowerCase());

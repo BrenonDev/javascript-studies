3f3f75d6d89f2cbc83fc365f23716ed1548b177f
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-02-11 13:39:39 -0300

    repetições

---
+++ 11 - Repetição e Iteração/scripts.js
@@ -0,0 +1,3 @@
+console.log("=== REPETIÇÕES ===");
+
+// Repetição é a execução de um bloco de código várias vezes de acordo com uma condição. Existem diferentes tipos de estruturas de repetição, como for, while e do...while.

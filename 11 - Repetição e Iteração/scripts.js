a1d106ba0253be8794ad7a0bca0a19fc987c3cd3
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-02-11 13:44:13 -0300

    loop while

--- 11 - Repetição e Iteração/scripts.js
+++ 11 - Repetição e Iteração/scripts.js
@@ -1,3 +1,21 @@
 console.log("=== REPETIÇÕES ===");
 
 // Repetição é a execução de um bloco de código várias vezes de acordo com uma condição. Existem diferentes tipos de estruturas de repetição, como for, while e do...while.
+
+
+// ======================================================================
+
+
+console.log("=== LOOP WHILE ===");
+
+// O loop while executa um bloco de código enquanto uma condição for verdadeira. A condição é verificada antes da execução do bloco, o que significa que o bloco pode não ser executado nenhuma vez se a condição for falsa desde o início.
+
+let count = 0;
+while (count < 5) {
+    console.log(count);
+    count++;
+}
+console.log("Contagem finalizada!");
+
+
+// ======================================================================

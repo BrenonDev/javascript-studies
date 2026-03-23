1120eca3f17aaf05b27e5495e3b75831dab1b97b
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-03-23 13:18:21 -0300

    definindo uma data e uma hora específica

--- 12 - Data e Hora/scripts.js
+++ 12 - Data e Hora/scripts.js
@@ -14,3 +14,17 @@ console.log(new Date(2020, 0, 1));
 
 
 // ======================================================================
+
+
+console.log("=== DEFININDO DATA E HORA ===");
+
+// Definindo uma data específica
+console.log(new Date(2025, 2, 23)); // 23 de março de 2025 (mês é zero-indexado, ou seja, 0 = janeiro, 1 = fevereiro, etc.)
+
+// Define data e hora específica
+console.log(new Date(2025, 2, 23, 13, 14, 15)); // 23 de março de 2025 às 13:14:15
+
+// Definindo uma data e hora específica usando uma string
+console.log(new Date("2025-03-23T13:14:15"));
+
+console.log(new Date("March 23, 2025 13:14:15"));

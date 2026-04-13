f37cefe21e18f1fe72a689bb3d5d74359e395303
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-04-13 13:07:05 -0300

    aprendendo sobre classes

---
+++ 13 - Classes/scripts.js
@@ -0,0 +1,14 @@
+console.log("=== APRENDENDO SOBRE CLASSES ===");
+
+// CLASSES
+// No JavaScript as classes são uma forma de criar objetos e definir seu comportamento por meio de construtores e métodos.
+// Elas foram introduzidas no ECMAScript 2015 (também conhecido como ES6) para fornecer uma sintaxe mais amigável para a criação de objetos e herança de protótipos (syntax sugar).
+
+// CONSTRUTORES E MÉTODOS
+// Uma classe é basicamente um modelo para criar objetos. Ela contém um construtor, que é um método especial chamado quando um objeto é instanciado a partir da classe.
+// Além do construtor, você pode adicionar métodos a uma classe. Métodos são funções associadas a objetos e descrevem o comportamento desses objetos.
+
+// HERANÇA
+// Uma classe pode herdar propriedades e métodos de outra classe, permitindo a reutilização de código.
+
+// ======================================================================

b5753ce90510a6ab7e7222e3158f4cca43fe4db2
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-06-22 13:33:36 -0300

    conhecendo o ECMAScript

---
+++ 14 - JavaScript Moderno/scripts.js
@@ -0,0 +1,20 @@
+console.log("=== CONHECENDO O ECMA SCRIPT ===");
+
+// 1995
+// O JavaScript foi criado em 1995 por Brendan Eich, enquanto ele trabalhava no Netscape.
+// O nome original da linguagem era Mocha, que ainda foi mudado para LiveScript, antes de a linguagem ser de fato nomeada como JavaScript.
+
+// 1996
+// No ano de 1996, com o objetivo de padronizar a linguagem a Netspace decidiu juntar o JavaScript à ECMA International.
+// AECMA (Associação Europeia dos Fabricantes de Computadores) é uma associação que realiza a especificação e padronização de sistemas da informação.
+// Com essa junção, foi criado um novo padrão de linguagem, que foi batizado com a união das palavras “ECMA” e “JavaScript”, resultando no nome “ECMAScript”.
+// Em resumo, JavaScript é a linguagem que você escreve e executa nos navegadores, enquanto ECMAScript é a especificação que define como a linguagem JavaScript deve se comportar.
+// As versões do ECMAScript (ES6, ES7, etc.) indicam as atualizações e novidades à linguagem ao longo do tempo.
+
+// Dias atuais
+// Sempre que há uma nova atualização dos recursos do JavaScript, ela é lançada pelo ECMAScript, que pode ser abreviada como ES.
+//É dessa especificação que veio siglas como: ECMAScript 2023, ES2023 ou ES14.
+// https://ecma-international.org/
+
+// ======================================================================
+

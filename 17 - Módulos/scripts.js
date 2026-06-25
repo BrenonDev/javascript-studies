b0ce2f7186f1b6f99c2a42358b980a3b3b2559e0
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-06-25 17:11:09 -0300

    conhecendo o ES Modules

---
+++ 17 - Módulos/scripts.js
@@ -0,0 +1,8 @@
+console.log("=== CONHECENDO O ES MODULES ===");
+
+// ES Modules
+// ESM é o sistema de módulo Javascript (ESM é uma abreviação para Módulos Javascript, em que “EcmaScript" é o nome oficial da especificação da linguagem Javascript).
+// Define um formato para organizar e estruturar o código em módulos, permitindo a modularização e reutilização de código.
+
+// ======================================================================
+

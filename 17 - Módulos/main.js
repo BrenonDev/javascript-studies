b67574e684584e38098182cd2aa99867fb19be8d
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-06-25 17:24:21 -0300

    importando tudo

--- 17 - Módulos/main.js
+++ 17 - Módulos/main.js
@@ -4,7 +4,9 @@ console.log("=== CONHECENDO O ES MODULES ===");
 // ESM é o sistema de módulo Javascript (ESM é uma abreviação para Módulos Javascript, em que “EcmaScript" é o nome oficial da especificação da linguagem Javascript).
 // Define um formato para organizar e estruturar o código em módulos, permitindo a modularização e reutilização de código.
 
+
 // ======================================================================
+
 
 console.log("=== CRIANDO MÓDULOS ===");
 
@@ -24,9 +26,23 @@ console.log("=== CRIANDO MÓDULOS ===");
 // Alternativa para exportar tudo no final
 // export { sum, multiply }
 
-// 3. Importando as funções para o novo arquivo JavaScript e chamando as funções
+// 3. Importando as funções individualmente para o novo arquivo JavaScript e chamando as funções
 
 import { sum, multiply } from "./calc.js";
 
 console.log("5 + 2 = " + sum(5, 2));
 console.log("5 x 2 = " + multiply(5, 2));
+
+
+// ======================================================================
+
+
+console.log("=== IMPORTANDO TUDO ===");
+
+import * as calc from "./calc.js";
+
+console.log("5 + 2 = " + calc.sum(5, 2));
+console.log("5 x 2 = " + calc.multiply(5, 2));
+
+
+// ======================================================================

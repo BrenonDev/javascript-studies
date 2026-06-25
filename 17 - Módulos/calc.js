dcbe7fc0540b86e667c35e4e7a1593c1924b7512
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-06-25 17:17:25 -0300

    criando módulos

---
+++ 17 - Módulos/calc.js
@@ -0,0 +1,10 @@
+export function sum(a, b) {
+    return a + b
+};
+
+export function multiply(a, b) {
+    return a * b
+};
+
+// Alternativa para exportar tudo no final
+// export { sum, multiply }
---
+++ 17 - Módulos/main.js
@@ -0,0 +1,32 @@
+console.log("=== CONHECENDO O ES MODULES ===");
+
+// ES Modules
+// ESM é o sistema de módulo Javascript (ESM é uma abreviação para Módulos Javascript, em que “EcmaScript" é o nome oficial da especificação da linguagem Javascript).
+// Define um formato para organizar e estruturar o código em módulos, permitindo a modularização e reutilização de código.
+
+// ======================================================================
+
+console.log("=== CRIANDO MÓDULOS ===");
+
+// 1. Definindo tipo do arquivo JavaScript para modular
+// <script type="module" src="main.js"></script>
+
+// 2. Criando um novo arquivo JavaScript e exportando as funções individualmente ou exportando tudo no final
+
+// export function sum(a, b) {
+//     return a + b
+// };
+
+// export function multiply(a, b) {
+//     return a * b
+// };
+
+// Alternativa para exportar tudo no final
+// export { sum, multiply }
+
+// 3. Importando as funções para o novo arquivo JavaScript e chamando as funções
+
+import { sum, multiply } from "./calc.js";
+
+console.log("5 + 2 = " + sum(5, 2));
+console.log("5 x 2 = " + multiply(5, 2));

2b7a3a77cf5d86d034bea8aec63a54ca3151a1fc
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-01 09:37:35 -0300

    conhecendo o setInterval()

--- 18 - Funções Assíncronas/scripts.js
+++ 18 - Funções Assíncronas/scripts.js
@@ -4,7 +4,28 @@ console.log("=== CONHECENDO O SETTIMEOUT ===");
 
 setTimeout(() => {
 console.log("Olá")
-}, 3000); // Tempo em milissegundos
+}, 4000); // Tempo em milissegundos
+
+
+// ======================================================================
+
+
+console.log("=== CONHECENDO O SETINTERVAL ===");
+
+// setInterval() executa uma função a cada intervalo de tempo especificado
+
+let value = 3;
+
+const interval = setInterval(() => {
+  console.log(value--);
+
+  if (value === 0) {
+    console.log("Feliz Ano Novo!");
+
+    // Interrompe o intervalo de execuções
+    clearInterval(interval);
+  }
+}, 1000)
 
 
 // ======================================================================

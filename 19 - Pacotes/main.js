f947379c19efe4e270f1eece2b0187b77b646cba
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-01 16:48:24 -0300

    utilizando o day.js pelo browser

--- 19 - Pacotes/main.js
+++ 19 - Pacotes/main.js
@@ -8,8 +8,25 @@ console.log("=== O QUE SÃO PACOTES? ===");
 // DISTRIBUIÇÃO
 
 // Bons pacotes são mantidas pela comunidade ou por organizações dedicadas. Isso significa que elas são atualizadas para corrigir bugs, melhorar o desempenho e fornecer novos recursos.
-
 // Ao usar pacotes, você pode se beneficiar dessas atualizações sem ter que implementá-las você mesmo.
+
+
+// ======================================================================
+
+
+console.log("=== UTILIZANDO O DAY.JS PELO BROWSER ===");
+
+// https://day.js.org/docs/en/installation/browser
+
+// No HTML:
+
+// <!-- Adiciona pacote/biblioteca no projeto -->
+// <script src="https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js"></script>
+// <script type="module" src="main.js"></script>
+
+const now = dayjs();
+
+console.log(now.format("DD/MM/YYYY"));
 
 
 // ======================================================================

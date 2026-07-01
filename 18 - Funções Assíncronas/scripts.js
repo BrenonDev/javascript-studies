27b7c5899f86f06b2801cf12eeacd3a1f58b2d43
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-01 09:29:23 -0300

    conhecendo o setTimeout()

---
+++ 18 - Funções Assíncronas/scripts.js
@@ -0,0 +1,10 @@
+console.log("=== CONHECENDO O SETTIMEOUT ===");
+
+// SetTimeout() executa uma função após um intervalo de tempo especificado
+
+setTimeout(() => {
+console.log("Olá")
+}, 3000); // Tempo em milissegundos
+
+
+// ======================================================================

1abab7826472e3d3fc78a66d6e458223e316de4b
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-02 10:26:00 -0300

    o que é uma API?

---
+++ 20 - Working APIs/scripts.js
@@ -0,0 +1,44 @@
+console.log("=== O QUE É UMA API? ===");
+
+// Uma API (Application Programming Interface) é uma Interface que disponibiliza um conjunto de funcionalidades para serem utilizadas.
+
+// Exemplos: API de CEP, previsão do tempo, cotação de moedas, etc.
+
+
+// RELAÇÃO CLIENTE E SERVIDOR
+
+// Cliente  ===requisição==>  API
+// Cliente  <===resposta====  API
+
+
+// UTILIZANDO UMA API
+
+// A    rota = caminho    B
+
+// APP    https://meuservidor.com.br/products    API
+
+// https => Protocolo de comunicação
+// meuservidor.com.br => Endereço do servidor
+// products => Recurso da API
+
+
+// MÉTODOS HTTP
+
+// GET => Leitura
+// POST => Criação
+// PUT => Atualização
+// DELETE => Deleção
+// PATCH => Atualizar parcial
+
+
+// JSON
+
+// O JSON (JavaScript Object Notation, significa JavaScript Object Notation. É uma notação de objetos utilizado para representar dados.
+// É amplamente utilizado na comunicação entre servidores e clientes.
+
+// {
+//     "chave": "valor",
+// }
+
+
+// ======================================================================
